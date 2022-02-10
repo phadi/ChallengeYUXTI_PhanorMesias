4199ba82587237cde704b379bf8b2ab1e5c72cab
pmesias <phanor.mesias00@hotmail.com>
2022-02-10 11:13:28 -0500

    Primer commit. Crea controlador para el Challenge

---
+++ Challenge_Back/Controllers/ChallengeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Challenge_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChallengeController : ControllerBase
+    {
+    }
+}
