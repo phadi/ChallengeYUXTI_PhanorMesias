35e5400a34ce8f42c9fbee80029d2247559094a6
pmesias <phanor.mesias00@hotmail.com>
2022-02-10 14:36:55 -0500

    Crea Modelo para Locations

---
+++ Challenge_Back/Controllers/LocationsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Challenge_Back.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Challenge_Back.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        // GET: api/<LocationsController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<Location> locations = new List<Location>();
+            Location location = new Location();
+            location.Name = "Name 1";
+            location.Availability = 15;
+            locations.Add(location);
+
+            JsonResult jsonResult = new JsonResult(locations);
+            jsonResult.StatusCode = (int)System.Net.HttpStatusCode.OK;
+
+
+            return jsonResult;
+        }
+
+        // GET api/<LocationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LocationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LocationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LocationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Challenge_Back/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_Back.Models
+{
+    public class Location
+    {
+        //public DateTime Date { get; set; }
+
+        public string Name { get; set; }
+
+        public int Availability { get; set; }
+    }
+}
